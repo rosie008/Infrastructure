d3d9fe9c741ff7de008e37acbaf9bb682df3785c
riyadis <riyadis008@gmail.com>
2026-05-20 21:07:27 +0700

    add nat configuration

--- terraform/modules/networking/route_table.tf
+++ terraform/modules/networking/route_table.tf
@@ -17,3 +17,21 @@ resource "aws_route_table_association" "public_association" {
   route_table_id = aws_route_table.public.id
 }
 
+#private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
